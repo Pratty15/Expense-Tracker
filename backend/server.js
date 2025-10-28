a587aec70dcd0b8565c319d057ec75a158edb33d
Pratty15 <usususuus66@gmail.com>
2025-10-28 16:24:00 +0530

    Auto update

--- backend/server.js
+++ backend/server.js
@@ -30,4 +30,3 @@ app.use("/api/expenses", expenseRoutes);
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-//tohar moc 
